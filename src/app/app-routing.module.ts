d2d443c037425fb6218dbec70ad49bc3ebec7d36
Alex Arhuata <alearhuatat@upt.pe>
2023-08-09 12:32:15 -0500

    404 component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PresentationComponent } from './presentation/presentation.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', component: PresentationComponent },
-  {path: 'projects', component: ProjectsComponent }
+  {path: 'projects', component: ProjectsComponent },
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent }
 ];
 
 @NgModule({
