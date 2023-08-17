3390793adf992f0d749d09704cd0b8a339ea206e
Alex Arhuata <alearhuatat@upt.pe>
2023-08-17 11:01:43 -0500

    refactor in components

---
+++ src/app/config.ts
@@ -0,0 +1,21 @@
+export const socialMedia = [
+    {
+        name: 'Github',
+        url: 'https://github.com/alxcript',
+    },
+    {
+        name: 'Email',
+        url: 'mailto:alearhuatat@upt.pe',
+    },
+];
+
+export const menu = [
+    {
+      name: 'Inicio',
+      url: '/',
+    },
+    {
+      name: 'Proyectos',
+      url: '/projects',
+    },
+  ];
